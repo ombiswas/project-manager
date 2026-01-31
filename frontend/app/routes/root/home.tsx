fa33498726a19764eb7a233eda66b01a855008c7
ombiswas <ombiswas345@gmail.com>
2026-01-31 23:01:54 +0530

    setting up project

---
+++ frontend/app/routes/root/home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import type { Route } from "../../+types/root";
+import { Button } from "@/components/ui/button";
+
+export function meta({ }: Route.MetaArgs) {
+    return [
+        { title: "Task Hub" },
+        { name: "description", content: "Welcome to React Router!" },
+    ];
+}
+
+
+const Homepage = () => {
+    return (
+        <div><Button>Click Me!</Button></div>
+    )
+}
+
+export default Homepage
