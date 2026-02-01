a9050af48b05bcb86a859729b544b0eb07bd852a
ombiswas <ombiswas345@gmail.com>
2026-02-01 12:26:30 +0530

    setting up project

--- frontend/app/routes/root/home.tsx
+++ frontend/app/routes/root/home.tsx
@@ -1,18 +1,25 @@
-import React from "react";
-import type { Route } from "../../+types/root";
+import React from 'react'
+import type { Route } from '../../+types/root';
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 export function meta({ }: Route.MetaArgs) {
     return [
-        { title: "Task Hub" },
+        { title: "TaskHub" },
         { name: "description", content: "Welcome to React Router!" },
     ];
 }
 
-
 const Homepage = () => {
     return (
-        <div><Button>Click Me!</Button></div>
+        <div className='w-full h-screen flex items-center justify-center gap-4'>
+            <Link to="/sign-in"><Button className='bg-blue-500 text-white'>
+                Login
+            </Button></Link>
+            <Link to="/sign-up"><Button variant="outline" className='bg-blue-500 text-white'>
+                Sign up
+            </Button></Link>
+        </div>
     )
 }
 
