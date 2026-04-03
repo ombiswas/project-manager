e538d1a2b4f3b3854b56de11a8bb3c12cbcd8804
ombiswas <ombiswas345@gmail.com>
2026-04-03 08:52:57 +0530

    dashboard layout

---
+++ frontend/app/components/layout/sidebar-component.tsx
@@ -0,0 +1,104 @@
+import { cn } from "@/lib/utils";
+import { useAuth } from "@/provider/auth-context";
+import type { Workspace } from "@/types";
+import { CheckCircle2, ChevronLeft, ChevronRight, LayoutDashboard, List, ListCheck, LogOut, Settings, Users, Wrench } from "lucide-react";
+import { useState } from "react";
+import { href, Link } from "react-router";
+import { Button } from "../ui/button";
+import { ScrollArea } from "../ui/scroll-area";
+import { SidebarNav } from "./sidebar-nav";
+
+export const SidebarComponent = ({
+    currentWorkspace
+}: { currentWorkspace: Workspace | null }) => {
+    const { user, logout } = useAuth();
+    const [isCollapsed, setIsCollapsed] = useState(false);
+
+    const navItems = [
+        {
+            title: "Dashboard",
+            href: "/dashboard",
+            icon: LayoutDashboard,
+        },
+        {
+            title: "Workspace",
+            href: "/workspace",
+            icon: Users,
+        },
+        {
+            title: "My Tasks",
+            href: "/my-tasks",
+            icon: ListCheck,
+        },
+        {
+            title: "Dashboard",
+            href: "/dashboard",
+            icon: LayoutDashboard,
+        },
+        {
+            title: "Members",
+            href: "/members",
+            icon: Users,
+        },
+        {
+            title: "Archived",
+            href: "/archived",
+            icon: CheckCircle2,
+        },
+        {
+            title: "Settings",
+            href: "/settings",
+            icon: Settings,
+        },
+    ];
+
+    return (
+        <div
+            className={cn("flex flex-col border-r bg-sidebar transition-all duration-300",
+                isCollapsed ? "w-16 md:w-[80px]" : "w-16 md:w-[240px]"
+            )}
+        >
+            <div className="flex h-14 items-center border-b mb-4 px-4">
+                <Link to="/dashboard" className="flex items-center ">
+                    {!isCollapsed && (
+                        <div className="flex items-center gap-2">
+                            <Wrench className="size-6 text-blue-600" />
+                            <span className="font-semibold text-lg hidden md:block">
+                                TaskHub
+                            </span>
+                        </div>
+                    )}
+
+                    {isCollapsed && <Wrench className="size-6 text-blue-600" />}
+                </Link>
+
+                <Button
+                    variant={"ghost"}
+                    size="icon"
+                    className="ml-auto hidden md:block"
+                    onClick={() => setIsCollapsed(!isCollapsed)}
+                >
+                    {isCollapsed ? (
+                        <ChevronRight className="size-4" />
+                    ) : (
+                        <ChevronLeft className="size-4 " />
+                    )}
+                </Button>
+            </div>
+            <ScrollArea className="flex-1 px-3 py-2">
+                <SidebarNav
+                    items={navItems}
+                    isCollapsed={isCollapsed}
+                    className={cn(isCollapsed && "items-center space-y-2")}
+                    currentWorkspace={currentWorkspace}
+                />
+            </ScrollArea>
+            <div>
+                <Button variant={"ghost"} size={isCollapsed ? "icon" : "default"} onClick={logout}>
+                    <LogOut className={cn("size-4", isCollapsed && "mr-2")} />
+                    <span className="hidden md:block">Logout</span>
+                </Button>
+            </div>
+        </div>
+    )
+};
--- frontend/app/routes/dashboard/dashboard-layout.tsx
+++ frontend/app/routes/dashboard/dashboard-layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/layout/header";
+import { SidebarComponent } from "@/components/layout/sidebar-component";
 import { Loader } from "@/components/loader";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/provider/auth-context";
@@ -24,22 +25,26 @@ const DashboardLayout = () => {
         setCurrentWorkspace(workspace);
     };
 
-    return <div className="flex h-screen w-full">{/* <SidebarComponent/> */}
-        <div className="flex flex-1 flex-col h-full">
-            <Header
-                onWorkspaceSelected={handleWorkspaceSelected}
-                selectedWorkspace={currentWorkspace}
-                onCreateWorkspace={() => setIsCreatingWorkspace(true)}
-            />
+    return (
+        <div className="flex h-screen w-full">
+            <SidebarComponent currentWorkspace={currentWorkspace} />
 
-            <main className="flex-1 overflow-y-auto h-full w-full">
-                <div className="mx-auto container px-2 sm:px-6 lg:px-8 py-0 md:py-8 w-full h-full">
+            <div className="flex flex-1 flex-col h-full">
+                <Header
+                    onWorkspaceSelected={handleWorkspaceSelected}
+                    selectedWorkspace={currentWorkspace}
+                    onCreateWorkspace={() => setIsCreatingWorkspace(true)}
+                />
 
-                </div>
-                <Outlet />
-            </main>
+                <main className="flex-1 overflow-y-auto h-full w-full">
+                    <div className="mx-auto container px-2 sm:px-6 lg:px-8 py-0 md:py-8 w-full h-full">
+                        <Outlet />
+                    </div>
+                </main>
+            </div>
+
         </div>
-    </div>;
+    );
 };
 
 export default DashboardLayout;
